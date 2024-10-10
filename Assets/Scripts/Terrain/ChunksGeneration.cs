5e4bc5a1f5ffa07ec678444089094370b2969ab5
Boushi33 <A.JOURDANNAUD@lahorde.school>
2024-10-10 17:25:01 +0200

    Added a shader to change color depending of height.

---
+++ Assets/Scripts/Terrain/ChunksGeneration.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Terrain
+{
+    public class ChunksGeneration : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+        
+        
+    }
+  
+}
